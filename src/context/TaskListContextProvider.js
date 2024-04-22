09935c2df787442a25460d278489835511d71d0c
CoderAbhinav <abhinavrb.a02@gmail.com>
2024-04-22 13:43:02 +0530

    Feature: Persistant data storage of tasks on client side
    Description:
    This commit introduces feature to store the tasks in the client side local storage

--- src/context/TaskListContextProvider.js
+++ src/context/TaskListContextProvider.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import TaskListContext from './TaskListContext';
 
 function TaskListContextProvider({ children }) {
-  const [taskList, setTaskList] = useState([]);
+
+  const retriveTaskListFromLocalStorage = ( ) => {
+    let taskListFromLocalStorage = localStorage.getItem( 'tasklist' );
+    console.log( 'TaskList:' +  taskListFromLocalStorage );
+    return JSON.parse( taskListFromLocalStorage ) || [];
+  }
+
+  const [taskList, setTaskList] = useState( retriveTaskListFromLocalStorage() );
+
+  useEffect(() => {
+    console.log( 'Storing In Local Storage: ' + JSON.stringify( taskList ) );
+    localStorage.setItem( 'tasklist', JSON.stringify( taskList ) );
+  }, [taskList]);
 
   return (
     <TaskListContext.Provider value={{ taskList, setTaskList }}>
